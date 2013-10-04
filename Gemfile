047249083bcf01cc44425a4561ffb715ab65b1fb
The Hexx <sensitivejapaneseman@gmail.com>
2013-10-04 19:52:43 +0900

    Github Pages使うのやめて、S3でやることにした。

--- Gemfile
+++ Gemfile
@@ -2,8 +2,9 @@ source 'http://rubygems.org'
 
 gem 'middleman', '~>3.1.5'
 gem 'middleman-blog', '~> 3.3.0'
-gem 'middleman-deploy', '~> 0.1.3'
+# gem 'middleman-deploy', '~> 0.1.3'
+gem "middleman-sync", "~> 3.0.12"
 gem 'slim', '~> 2.0.0'
 
 # For feed.xml.builder
-gem 'builder', '~> 3.0'
+# gem 'builder', '~> 3.0'
--- config.rb
+++ config.rb
@@ -2,24 +2,35 @@
 Time.zone = 'Tokyo'
 
 activate :blog do |blog|
-  blog.prefix = 'blog'
-  blog.sources = 'articles/:year-:month-:day-:title.html'
-  blog.layout = 'article'
-  blog.tag_template = 'blog/tag.html'
-  blog.calendar_template = 'blog/calendar.html'
+  # blog.prefix            = 'blog'
+  blog.sources           = 'articles/:year-:month-:day-:title.html'
+  blog.layout            = 'article'
+  blog.tag_template      = 'tag.html'
+  blog.calendar_template = 'calendar.html'
 end
 
 page 'feed.xml', :layout => false
 
 require 'slim'
 
-set :css_dir, 'blog/stylesheets'
-set :js_dir, 'blog/javascripts'
-set :images_dir, 'blog/images'
+set :css_dir, 'stylesheets'
+set :js_dir, 'javascripts'
+set :images_dir, 'images'
 
-activate :deploy do |deploy|
-  deploy.method = :git
-  deploy.branch = 'gh-pages'
+# activate :deploy do |deploy|
+#   deploy.method = :git
+#   deploy.branch = 'gh-pages'
+# end
+
+activate :sync do |sync|
+  sync.fog_provider          = 'AWS'
+  sync.fog_directory         = 'blog.pab-tech.net'
+  sync.fog_region            = 'ap-northeast-1'
+  sync.aws_access_key_id     = ENV['AWS_ACCESS_KEY_ID']
+  sync.aws_secret_access_key = ENV['AWS_SECRET_ACCESS_KEY']
+  sync.existing_remote_files = 'keep'
+  sync.gzip_compression      = false
+  sync.after_build           = false
 end
 
 helpers do
